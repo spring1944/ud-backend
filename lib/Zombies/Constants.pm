5bb443776f1ed2edbf3f22b5020e4d3c022e34af
Ben Tyler <benjamin.tyler@gmail.com>
2015-02-16 21:07:00 +0100

    start money => 70k, minimum army size of 10 units

--- lib/Zombies/Constants.pm
+++ lib/Zombies/Constants.pm
@@ -7,9 +7,11 @@ use Exporter qw(import);
 our @EXPORT_OK = qw(
     STARTING_MONEY
     SELL_PENALTY
+    MIN_ARMY_SIZE
 );
 
-use constant STARTING_MONEY => 100_000;
+use constant STARTING_MONEY => 70_000;
 use constant SELL_PENALTY => 0.05;
+use constant MIN_ARMY_SIZE => 5;
 
 1;
